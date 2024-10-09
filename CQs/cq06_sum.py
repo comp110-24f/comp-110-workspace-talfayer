eaeab2c3bf9150a0ffa0acbc614acafb879c5487
tal.fayer <talfayer@unc.edu>
2024-10-09 08:57:57 -0400

    cq06

---
+++ CQs/cq06_sum.py
@@ -0,0 +1,29 @@
+"""Summing the elements of a list using different loops"""
+
+__author__ = "730646268"
+
+
+def w_sum(vals: list[float]) -> float:
+    index: int = 0
+    sum: float = 0.0
+    while index < len(vals):
+        if len(vals) == 0:
+            return 0.0
+        else:
+            sum += vals[index]
+            index += 1
+    return sum
+
+
+def f_sum(vals: list[float]) -> float:
+    sum: float = 0.0
+    for num in vals:
+        sum += num
+    return sum
+
+
+def f_range_sum(vals: list[float]) -> float:
+    sum: float = 0.0
+    for num in range(0, len(vals)):
+        sum += num
+    return sum
