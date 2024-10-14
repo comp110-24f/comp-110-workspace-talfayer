f144360b58c917e32bbf38298391f42faf762a52
tal.fayer <talfayer@unc.edu>
2024-10-14 11:36:02 -0400

    unit tests lesson comments

--- lessons/unit_tests/list_fns_test.py
+++ lessons/unit_tests/list_fns_test.py
@@ -6,12 +6,17 @@ def test_get_first() -> None:
     assert get_first(fruits) == "apples"
 
 
-def test_remove_first_ret_value() -> None:
+def test_remove_first_ret_value() -> None:  # testing the return value
     fruits: list[str] = ["apples", "oranges", "bananas"]
-    assert remove_first(fruits) == None
+    assert (
+        remove_first(fruits) == None
+    )  # has to equal none because the function has a RV of none
 
 
-def test_remove_first_behavior() -> None:
+def test_remove_first_behavior() -> None:  # testing the behavior
     fruits: list[str] = ["apples", "oranges", "bananas"]
     remove_first(fruits)
-    assert fruits == ["oranges", "bananas"]
+    assert fruits == [
+        "oranges",
+        "bananas",
+    ]  # equals this bc the function is meant to remove the first value but has return none, so this is testing without the return
